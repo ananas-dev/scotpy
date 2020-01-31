70560f7ac894fc9e3925c356b7d06f82ed104359
ananas-dev <46646129+ananas-dev@users.noreply.github.com>
2020-01-31 09:29:29 +0100

    Add files via upload

--- rules.py
+++ rules.py
@@ -4,8 +4,15 @@ import config
 
 # Number of turn algorithm:
 
-#(The number of turn is set to the number of nodes of the graph)
+# The number of turn is set to the number of nodes of the graph
 def setturns():
     turns = len(config.graph["nodes"])
     return(turns)
+
+# Check if the cops win algorithm:
+
+# It checks if alls the nodes adjacent to the thief are the same
+# then the nodes ajacent to the cops or their position
+
+#def checkifwin:
 
